4cb2cffa6e70466fb9a9c604e9b85ce688149643
iyas89 <iyasasaad@gmail.com>
2023-11-02 13:16:25 -0300

    rama developer

--- src/redux/provider.jsx
+++ src/redux/provider.jsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import axios from "axios";
 
 // axios.defaults.baseURL= 'http://localhost:3001';
+
 axios.defaults.baseURL= 'https://server-eventifypro.onrender.com';
 
 export function Providers({ children }) {
