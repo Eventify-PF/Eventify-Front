21453ab650984f982386f877a91e1c52210a3a7f
iyas89 <iyasasaad@gmail.com>
2023-11-02 12:38:25 -0300

    first

---
+++ src/redux/action/ticketActions.js
@@ -0,0 +1,9 @@
+import { CREATE_TICKET } from "../action-type/ticketConstants";
+import axios from "axios";
+
+export const createTicket = (ticket) => {
+    return async (dispatch) => {
+      const { data } = await axios.post("/events/ticket", ticket);
+      return dispatch({ type: CREATE_TICKET, payload: data });
+    };
+  };
