1596b200883b383f0e809217ca4a2c91490df1b5
iyas89 <iyasasaad@gmail.com>
2023-11-03 02:10:02 -0300

    NavBar + Ticket

--- src/components/Navbar/NavbarMenu.jsx
+++ src/components/Navbar/NavbarMenu.jsx
@@ -1,27 +1,24 @@
 import Link from "next/link";
-const NavbarMenu = () => {
-  const menuItems = ['Home', 'Event', 'About'];
-  const adminItems = ["Create-Event", "Create-Ticket"];
+const MenuItem = () => {
   return (
-    <div className="flex items-center justify-between">
-      <ul className="text-orange-500 text-lg hidden lg:flex items-center">
-        {menuItems.map((item) => (
-          <li key={item} className="py-2 px-8 flex">
-            <Link href={`/${item.toLowerCase()}`}>
-              {item}
-            </Link>
-          </li>
-        ))}
-
-        {adminItems.map((item) => (
-          <li key={item} className="py-2 px-8 flex">
-            <Link href={`/admin/${item.toLowerCase()}`}>{item}</Link>
-          </li>
-        ))}
+    <div className="flex items-center justify-between py-2">
+      <ul className="text-orange-500 text-xl hidden lg:flex items-center ">
+        <li className="py-2 px-8 flex">
+          <Link href={'/event'}>Events</Link>
+        </li>
+        <li className="py-2 px-8 flex">
+          <Link href={'/about'}>About</Link>
+        </li>
+        <li className="py-2 px-8 flex">
+          <Link href={'/admin/create-event'}>create Event</Link>
+        </li>
+        <li className="py-2 px-8 flex">
+          <Link href={'/admin/create-ticket'}>create Ticket</Link>
+        </li>
       </ul>
     </div>
-
   );
 }
 
-export default NavbarMenu;
+export default MenuItem;
+
