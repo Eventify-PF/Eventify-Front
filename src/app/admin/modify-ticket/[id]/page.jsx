d879943ea0a50ed312c5c531ed4e9813dfa92d09
iyas89 <iyasasaad@gmail.com>
2023-11-06 13:32:38 -0300

    mayuscula menuscula

--- src/app/admin/modify-ticket/[id]/page.jsx
+++ src/app/admin/modify-ticket/[id]/page.jsx
@@ -1,15 +1,15 @@
  
 import EditTicketForm from "./updateTicket";
-const loadTicket = async (id) => {
+const LoadTicket = async (id) => {
   const res = await fetch(`http://localhost:3001/events/${id}`);
   // const res = await fetch(`https://server-eventifypro.onrender.com/events/${id}`);
-  
+
   const data = await res.json();
   return data.tickets[0];
 };
 
 export default async function EventPage({ params }) {
-  const detailTicket = await loadTicket(params.id);
+  const detailTicket = await LoadTicket(params.id);
 
   return <EditTicketForm detailTicket={detailTicket} />;
 }
