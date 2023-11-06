9ce8a68ad77084392251c6b104aea6d0f0dbd78f
iyas89 <iyasasaad@gmail.com>
2023-11-06 12:40:36 -0300

    arreglo

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -6,7 +6,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 
-const page = () => {
+const EventPage = () => {
   const dispatch = useDispatch();
   const events = useSelector((state) => state.eventReducer.allEvents);
 
@@ -20,4 +20,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default EventPage;
